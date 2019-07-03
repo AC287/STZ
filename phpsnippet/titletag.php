19a61458407530787b7fe669a32839ecf0da5114
AC287 <arthurchen287@gmail.com>
2019-07-03 12:18:22 -0400

    added title tag and meta.

--- phpsnippet/titletag.php
+++ phpsnippet/titletag.php
@@ -3,7 +3,7 @@
     NOTES:
       This is a manual input.
       If new page or section is added, you will need to enter manually append below switch/case statement.
-      Then update ldrmetadesc spreadsheet.
+      Then update stzmetadesc spreadsheet.
   */
 
   // print_r($curLocationArr);
@@ -14,14 +14,14 @@
 
     case (empty($curLocationArr)):
       //GET HOME TITLE & META DESC TAG for live site.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='home';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='home';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('products',$curLocationArr) || in_array('search',$curLocationArr)):
       //GET PRODUCTS TITLE & META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='products';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='products';");
       switch ($curLocationArr) {
 
         case (((count($curLocationArr) <=2) && (in_array('products',$curLocationArr))) || (in_array('search',$curLocationArr))):
@@ -48,60 +48,60 @@
 
     case (in_array('about', $curLocationArr)):
       //GET ABOUT TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='about';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='about';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('industries', $curLocationArr)):
       //GET ABOUT TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='industries';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='industries';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('team', $curLocationArr)):
       //GET TEAM TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='team';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='team';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('brands', $curLocationArr)):
       //GET TEAM TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='brands';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='brands';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('career', $curLocationArr)):
       //GET TEAM TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='career';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='career';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('contact', $curLocationArr)):
       //GET TEAM TITLE AND META DESC TAG.
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='contact';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='contact';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case (in_array('tradeshows', $curLocationArr)):
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='tradeshows';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='tradeshows';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     case(count($curLocationArr)==3 && (gettype($curLocationArr[0])=='integer' && gettype($curLocationArr[1]=='integer'))):
-      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='tradeshows';");
+      $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='tradeshows';");
       echo "<title>STZ | ".$metatitleDB[0]->title."</title>";
       echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
 
     default:
-    $metatitleDB = $wpdb->get_results("SELECT * FROM wp_ldrmetadesc WHERE page='home';");
+    $metatitleDB = $wpdb->get_results("SELECT * FROM wp_stzmetadesc WHERE page='home';");
     echo "<title>STZ</title>";
     echo "<meta name='description' content='".htmlspecialchars($metatitleDB[0]->metadesc,ENT_QUOTES)."'>";
     break;
