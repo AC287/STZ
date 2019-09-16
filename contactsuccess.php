0e952c605cd825bab6ec8be1812795a62741ba39
AC287 <arthurchen287@gmail.com>
2019-09-16 08:52:49 -0400

    minor edit to message format.

--- contactsuccess.php
+++ contactsuccess.php
@@ -51,7 +51,7 @@
         $message = $_POST['contact-message'];
         $company = $_POST['contact-company'];
         $phone = $_POST['contact-phone'];
-        $contents = "Name: $name \nEmail: $email \nPhone: $phone \nCompany: $company \nMessage: $message";
+        $contents = "Name: $name \nEmail: $email \nPhone: $phone \nCompany: $company \nMessage: \n$message";
 
         //php mailer variables
         $to = "orders@stzindustries.com";
